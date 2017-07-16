267deeb87f5a595c312fd61b2a56658590f7ccf9
Péter Kovács <peter.kovacs0519@gmail.com>
2017-07-16 18:31:58 +0200

    LFLT-141 Spring Boot migration

--- web/src/main/java/hu/psprog/leaflet/lms/web/config/WebMVCConfiguration.java
+++ web/src/main/java/hu/psprog/leaflet/lms/web/config/WebMVCConfiguration.java
@@ -1,23 +1,14 @@
 package hu.psprog.leaflet.lms.web.config;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
-import nz.net.ultraq.thymeleaf.decorators.strategies.GroupingStrategy;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.servlet.ViewResolver;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.Map;
 
 /**
  * Spring Web MVC configuration.
@@ -26,62 +17,32 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
  */
 @Configuration
 @EnableWebMvc
-public class WebMVCConfiguration extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+public class WebMVCConfiguration extends WebMvcConfigurerAdapter {
 
-    private static final String CHARACTER_ENCODING = "UTF-8";
-
-    private ApplicationContext applicationContext;
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        templateEngine.addDialect(new LayoutDialect(new GroupingStrategy()));
-        templateEngine.addDialect(new SpringSecurityDialect());
-        templateEngine.setEnableSpringELCompiler(true);
-
-        return templateEngine;
-    }
-
-    @Bean
-    public ViewResolver thymeleafViewResolver() {
-
-        ThymeleafViewResolver thymeleafMainViewResolver = new ThymeleafViewResolver();
-        thymeleafMainViewResolver.setTemplateEngine(templateEngine());
-        thymeleafMainViewResolver.setCharacterEncoding(CHARACTER_ENCODING);
-        thymeleafMainViewResolver.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
-        return thymeleafMainViewResolver;
-    }
+    @Autowired
+    private WebAppResources webAppResources;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("/css/**")
-                .addResourceLocations("/resources/css/");
-
-        registry.addResourceHandler("/fonts/**")
-                .addResourceLocations("/resources/fonts/");
-
-        registry.addResourceHandler("/scripts/**")
-                .addResourceLocations("/resources/scripts/");
+        webAppResources.getResources()
+                .forEach((resourceHandler, resourceLocation) -> registry
+                        .addResourceHandler(resourceHandler)
+                        .addResourceLocations(resourceLocation));
     }
 
-    private ITemplateResolver templateResolver() {
+    @Component
+    @ConfigurationProperties(prefix = "webapp")
+    class WebAppResources {
 
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("/WEB-INF/template/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setCacheable(false);
+        private Map<String, String> resources;
 
-        return templateResolver;
+        public Map<String, String> getResources() {
+            return resources;
+        }
+
+        public void setResources(Map<String, String> resources) {
+            this.resources = resources;
+        }
     }
 }
