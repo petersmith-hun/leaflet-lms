1b6b6ec7449d2549c5463062fdc2993305a569db
Péter Kovács <peter.kovacs0519@gmail.com>
2017-11-13 20:16:01 +0100

    LFLT-186 Implement Tags controller in LMS

--- service/src/main/java/hu/psprog/leaflet/lms/service/facade/TagFacade.java
+++ service/src/main/java/hu/psprog/leaflet/lms/service/facade/TagFacade.java
@@ -1,5 +1,6 @@
 package hu.psprog.leaflet.lms.service.facade;
 
+import hu.psprog.leaflet.api.rest.request.tag.TagCreateRequestModel;
 import hu.psprog.leaflet.api.rest.response.entry.EditEntryDataModel;
 import hu.psprog.leaflet.api.rest.response.tag.TagDataModel;
 import hu.psprog.leaflet.bridge.client.exception.CommunicationFailureException;
@@ -31,4 +32,48 @@ public interface TagFacade {
      * @throws CommunicationFailureException if Bridge cannot reach Leaflet
      */
     void handleAssignmentsOnChange(ModifyEntryRequest modifyEntryRequest, EditEntryDataModel editEntryDataModel) throws CommunicationFailureException;
+
+    /**
+     * Returns details of the tag identified by given ID.
+     *
+     * @param tagID ID of tag to retrieve details of
+     * @return tag data as {@link TagDataModel}
+     * @throws CommunicationFailureException if Bridge cannot reach Leaflet
+     */
+    TagDataModel getTag(Long tagID) throws CommunicationFailureException;
+
+    /**
+     * Processes tag creation request.
+     *
+     * @param tagCreateRequestModel tag data
+     * @return ID of the created tag
+     * @throws CommunicationFailureException if Bridge cannot reach Leaflet
+     */
+    Long processCreateTag(TagCreateRequestModel tagCreateRequestModel) throws CommunicationFailureException;
+
+    /**
+     * Processes tag update request.
+     *
+     * @param tagID ID of the tag to update
+     * @param tagCreateRequestModel update tag data
+     * @throws CommunicationFailureException if Bridge cannot reach Leaflet
+     */
+    void processEditTag(Long tagID, TagCreateRequestModel tagCreateRequestModel) throws CommunicationFailureException;
+
+    /**
+     * Processes tag status change request.
+     *
+     * @param tagID ID of the tag to update
+     * @return current status as boolean, {@code true} if enabled, {@code false} otherwise
+     * @throws CommunicationFailureException if Bridge cannot reach Leaflet
+     */
+    boolean processStatusChange(Long tagID) throws CommunicationFailureException;
+
+    /**
+     * Processes tag deletion request.
+     *
+     * @param tagID ID of the tag to delete
+     * @throws CommunicationFailureException if Bridge cannot reach Leaflet
+     */
+    void processDeleteTag(Long tagID) throws CommunicationFailureException;
 }
---
+++ web/src/main/java/hu/psprog/leaflet/lms/web/controller/TagsController.java
@@ -0,0 +1,183 @@
+package hu.psprog.leaflet.lms.web.controller;
+
+import hu.psprog.leaflet.api.rest.request.tag.TagCreateRequestModel;
+import hu.psprog.leaflet.bridge.client.exception.CommunicationFailureException;
+import hu.psprog.leaflet.lms.service.facade.TagFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ * Tag management controller for LMS.
+ *
+ * @author Peter Smith
+ */
+@Controller
+@RequestMapping(TagsController.PATH_TAGS)
+public class TagsController extends BaseController {
+
+    private static final String VIEW_TAGS_LIST = "view/tags/list";
+    private static final String VIEW_TAGS_EDIT_FORM = "view/tags/edit_form";
+    private static final String VIEW_TAGS_DETAILS = "view/tags/details";
+
+    private static final String TAG_SUCCESSFULLY_CREATED = "Tag successfully created.";
+    private static final String TAG_SUCCESSFULLY_UPDATED = "Tag successfully updated.";
+    private static final String TAG_SUCCESSFULLY_DELETED = "Tag successfully deleted.";
+    private static final String TAG_STATUS_SUCCESSFULLY_UPDATED = "Tag status successfully updated to %s";
+
+    static final String PATH_TAGS = "/tags";
+
+    private TagFacade tagFacade;
+
+    @Autowired
+    public TagsController(TagFacade tagFacade) {
+        this.tagFacade = tagFacade;
+    }
+
+    /**
+     * Returns list of existing tags.
+     *
+     * @return populated {@link ModelAndView} object
+     * @throws CommunicationFailureException on Bridge communication failure
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView listTags() throws CommunicationFailureException {
+
+        return modelAndViewFactory
+                .createForView(VIEW_TAGS_LIST)
+                .withAttribute("tags", tagFacade.getAllTags())
+                .build();
+    }
+
+    /**
+     * Returns tag identified by given ID.
+     *
+     * @param tagID ID of the tag to retrieve
+     * @return populated {@link ModelAndView} object
+     * @throws CommunicationFailureException on Bridge communication failure
+     */
+    @RequestMapping(method = RequestMethod.GET, path = PATH_VIEW)
+    public ModelAndView viewTag(@PathVariable(PATH_VARIABLE_ID) Long tagID) throws CommunicationFailureException {
+
+        return modelAndViewFactory
+                .createForView(VIEW_TAGS_DETAILS)
+                .withAttribute("tag", tagFacade.getTag(tagID))
+                .build();
+    }
+
+    /**
+     * Renders tag creation form.
+     *
+     * @return populated {@link ModelAndView} object
+     */
+    @RequestMapping(method = RequestMethod.GET, path = PATH_CREATE)
+    public ModelAndView showCreateTagForm() {
+
+        return modelAndViewFactory
+                .createForView(VIEW_TAGS_EDIT_FORM)
+                .build();
+    }
+
+    /**
+     * Processes tag creation request.
+     *
+     * @param tagCreateRequestModel tag data
+     * @param redirectAttributes redirection attributes
+     * @return populated {@link ModelAndView} object (redirection to created tag)
+     * @throws CommunicationFailureException on Bridge communication failure
+     */
+    @RequestMapping(method = RequestMethod.POST, path = PATH_CREATE)
+    public ModelAndView processCreateTag(@ModelAttribute TagCreateRequestModel tagCreateRequestModel,
+                                         RedirectAttributes redirectAttributes)
+            throws CommunicationFailureException {
+
+        Long createdID = tagFacade.processCreateTag(tagCreateRequestModel);
+        redirectAttributes.addFlashAttribute(FLASH_MESSAGE, TAG_SUCCESSFULLY_CREATED);
+
+        return modelAndViewFactory.createRedirectionTo(getRedirectionPath(createdID));
+    }
+
+    /**
+     * Renders tag edit form.
+     *
+     * @param tagID ID of the tag to edit
+     * @return populated {@link ModelAndView} object
+     * @throws CommunicationFailureException on Bridge communication failure
+     */
+    @RequestMapping(method = RequestMethod.GET, path = PATH_EDIT)
+    public ModelAndView showEditTagForm(@PathVariable(PATH_VARIABLE_ID) Long tagID) throws CommunicationFailureException {
+
+        return modelAndViewFactory
+                .createForView(VIEW_TAGS_EDIT_FORM)
+                .withAttribute("tag", tagFacade.getTag(tagID))
+                .build();
+    }
+
+    /**
+     * Processes tag edit request.
+     *
+     * @param tagID ID of the tag to edit
+     * @param tagCreateRequestModel update tag data
+     * @param redirectAttributes redirection attributes
+     * @return populated {@link ModelAndView} object (redirection to edited tag)
+     * @throws CommunicationFailureException on Bridge communication failure
+     */
+    @RequestMapping(method = RequestMethod.POST, path = PATH_EDIT)
+    public ModelAndView processEditTag(@PathVariable(PATH_VARIABLE_ID) Long tagID,
+                                       @ModelAttribute TagCreateRequestModel tagCreateRequestModel,
+                                       RedirectAttributes redirectAttributes)
+            throws CommunicationFailureException {
+
+        tagFacade.processEditTag(tagID, tagCreateRequestModel);
+        redirectAttributes.addFlashAttribute(FLASH_MESSAGE, TAG_SUCCESSFULLY_UPDATED);
+
+        return modelAndViewFactory.createRedirectionTo(getRedirectionPath(tagID));
+    }
+
+    /**
+     * Processes tag deletion request.
+     *
+     * @param tagID ID of the tag to delete
+     * @param redirectAttributes redirection attributes
+     * @return populated {@link ModelAndView} object (redirection to tag list)
+     * @throws CommunicationFailureException on Bridge communication failure
+     */
+    @RequestMapping(method = RequestMethod.POST, path = PATH_DELETE)
+    public ModelAndView processDeleteTag(@PathVariable(PATH_VARIABLE_ID) Long tagID, RedirectAttributes redirectAttributes)
+            throws CommunicationFailureException {
+
+        tagFacade.processDeleteTag(tagID);
+        redirectAttributes.addFlashAttribute(FLASH_MESSAGE, TAG_SUCCESSFULLY_DELETED);
+
+        return modelAndViewFactory.createRedirectionTo(PATH_TAGS);
+    }
+
+    /**
+     * Processes tag status change request.
+     *
+     * @param tagID ID of the tag to update status of
+     * @param redirectAttributes redirection attributes
+     * @return populated {@link ModelAndView} object (redirection to edited tag)
+     * @throws CommunicationFailureException on Bridge communication failure
+     */
+    @RequestMapping(method = RequestMethod.POST, path = PATH_STATUS)
+    public ModelAndView processStatusChange(@PathVariable(PATH_VARIABLE_ID) Long tagID, RedirectAttributes redirectAttributes)
+            throws CommunicationFailureException {
+
+        String currentStatus = tagFacade.processStatusChange(tagID)
+                ? "enabled"
+                : "disabled";
+        redirectAttributes.addFlashAttribute(FLASH_MESSAGE, String.format(TAG_STATUS_SUCCESSFULLY_UPDATED, currentStatus));
+
+        return modelAndViewFactory.createRedirectionTo(getRedirectionPath(tagID));
+    }
+
+    private String getRedirectionPath(Long tagID) {
+        return PATH_TAGS + replaceIDInViewPath(tagID);
+    }
+}
