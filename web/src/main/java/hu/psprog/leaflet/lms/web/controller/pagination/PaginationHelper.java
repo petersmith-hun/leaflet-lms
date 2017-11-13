6dafb8362f869c239ba4139d0a13397bbe4ad7e5
Péter Kovács <peter.kovacs0519@gmail.com>
2017-11-13 18:40:35 +0100

    LFLT-185 Implement Comments controller in LMS

--- web/src/main/java/hu/psprog/leaflet/lms/web/controller/pagination/PaginationHelper.java
+++ web/src/main/java/hu/psprog/leaflet/lms/web/controller/pagination/PaginationHelper.java
@@ -2,8 +2,6 @@ package hu.psprog.leaflet.lms.web.controller.pagination;
 
 import hu.psprog.leaflet.bridge.client.domain.OrderDirection;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
@@ -123,36 +121,4 @@ public abstract class PaginationHelper<T extends Enum> {
                 .map(valueMapper);
     }
 
-    @Component
-    @ConfigurationProperties(prefix = "pagination.default")
-    class PaginationDefaults {
-
-        private Integer limit;
-        private OrderDirection orderDirection;
-        private String orderBy;
-
-        public Integer getLimit() {
-            return limit;
-        }
-
-        public void setLimit(Integer limit) {
-            this.limit = limit;
-        }
-
-        public OrderDirection getOrderDirection() {
-            return orderDirection;
-        }
-
-        public void setOrderDirection(OrderDirection orderDirection) {
-            this.orderDirection = orderDirection;
-        }
-
-        public String getOrderBy() {
-            return orderBy;
-        }
-
-        public void setOrderBy(String orderBy) {
-            this.orderBy = orderBy;
-        }
-    }
 }
