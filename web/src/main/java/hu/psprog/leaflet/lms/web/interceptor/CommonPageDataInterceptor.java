2c002b4de9fce00cdd5854827e1c183f6f69409d
Péter Kovács <peter.kovacs0519@gmail.com>
2019-05-18 17:15:39 +0200

    LFLT-79 Management System User handling UI  - Fixed appending URL with app version on redirection  - Fixed formatting of Controls table column  - Redesigned user management UI

--- web/src/main/java/hu/psprog/leaflet/lms/web/interceptor/CommonPageDataInterceptor.java
+++ web/src/main/java/hu/psprog/leaflet/lms/web/interceptor/CommonPageDataInterceptor.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -19,23 +21,28 @@ import java.util.Objects;
 public class CommonPageDataInterceptor extends HandlerInterceptorAdapter {
 
     private static final String APP_VERSION = "${app.version}";
-    private static final String APP_VERSION_ATTRIBUTE = "app_version";
+    private static final String APP_VERSION_ATTRIBUTE = "footerAppVersion";
+    private static final String COMMON_ATTRIBUTE = "common";
 
-    private String applicationVersion;
+    private Map<String, String> commonPageData;
 
     @Autowired
     public CommonPageDataInterceptor(@Value(APP_VERSION) String applicationVersion) {
-        this.applicationVersion = applicationVersion;
+        commonPageData = Map.of(APP_VERSION_ATTRIBUTE, applicationVersion);
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        if (Objects.nonNull(modelAndView)) {
+        if (isRequestDispatcher(request) && Objects.nonNull(modelAndView)) {
             setApplicationVersion(modelAndView);
         }
     }
 
+    private boolean isRequestDispatcher(HttpServletRequest request) {
+        return request.getDispatcherType() == DispatcherType.REQUEST;
+    }
+
     private void setApplicationVersion(ModelAndView modelAndView) {
-        modelAndView.addObject(APP_VERSION_ATTRIBUTE, applicationVersion);
+        modelAndView.addObject(COMMON_ATTRIBUTE, commonPageData);
     }
 }
--- web/src/test/java/hu/psprog/leaflet/lms/web/interceptor/CommonPageDataInterceptorTest.java
+++ web/src/test/java/hu/psprog/leaflet/lms/web/interceptor/CommonPageDataInterceptorTest.java
@@ -3,11 +3,20 @@ package hu.psprog.leaflet.lms.web.interceptor;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.Map;
+
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
 
 /**
  * Unit tests for {@link CommonPageDataInterceptor}.
@@ -18,11 +27,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class CommonPageDataInterceptorTest {
 
     private static final String APPLICATION_VERSION = "appVersion";
+    private static final Map<String, String> COMMON_PAGE_DATA_MAP = Map.of("footerAppVersion", APPLICATION_VERSION);
 
     private CommonPageDataInterceptor commonPageDataInterceptor;
 
+    @Mock
+    private HttpServletRequest request;
+
     @Before
     public void setup() {
+        MockitoAnnotations.initMocks(this);
         commonPageDataInterceptor = new CommonPageDataInterceptor(APPLICATION_VERSION);
     }
 
@@ -31,20 +45,39 @@ public class CommonPageDataInterceptorTest {
 
         // given
         ModelAndView modelAndView = new ModelAndView();
+        given(request.getDispatcherType()).willReturn(DispatcherType.REQUEST);
 
         // when
-        commonPageDataInterceptor.postHandle(null, null, null, modelAndView);
+        commonPageDataInterceptor.postHandle(request, null, null, modelAndView);
 
         // then
         assertThat(modelAndView.getModel().size(), equalTo(1));
-        assertThat(modelAndView.getModelMap().get("app_version"), equalTo(APPLICATION_VERSION));
+        assertThat(modelAndView.getModelMap().get("common"), equalTo(COMMON_PAGE_DATA_MAP));
+    }
+
+
+    @Test
+    public void shouldPostHandleDoNothingIfDispatcherTypeIsNotRequest() throws Exception {
+
+        // given
+        ModelAndView modelAndView = new ModelAndView();
+        given(request.getDispatcherType()).willReturn(DispatcherType.ERROR);
+
+        // when
+        commonPageDataInterceptor.postHandle(request, null, null, modelAndView);
+
+        // then
+        assertThat(modelAndView.isEmpty(), is(true));
     }
 
     @Test
     public void shouldPostHandleDoNothingIfModelAndViewIsNull() throws Exception {
 
+        // given
+        given(request.getDispatcherType()).willReturn(DispatcherType.REQUEST);
+
         // when
-        commonPageDataInterceptor.postHandle(null, null, null, null);
+        commonPageDataInterceptor.postHandle(request, null, null, null);
 
         // then
         // do nothing
